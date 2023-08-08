912363ed3a06819d56e319a9f2b2b7af006c0547
Justinding765 <justin.ding@mail.utoronto.ca>
2023-08-08 12:32:47 -0400

    test

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,9 +4,9 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      //target: 'http://backend.mock-interviewer-and-job-application-database.local:5000',
+      target: 'http://backend.mock-interviewer-and-job-application-database.local:5000',
       //target: 'http://backend.justinding765.com:5000',
-      target: 'http://backend:5000',
+      //target: 'http://backend:5000',
       changeOrigin: true,
     })
   );
